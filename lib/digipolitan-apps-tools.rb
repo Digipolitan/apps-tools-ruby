4550e20b9dc29ea670bd518a9684ffc4a9a35d2d
Benoit BRIATTE <briatte.benoit@digipolitan.com>
2016-12-20 14:39:33 +0100

    remove ui

--- lib/digipolitan-apps-tools.rb
+++ lib/digipolitan-apps-tools.rb
@@ -1,6 +1,5 @@
 #!/usr/bin/ruby
 
-require 'digipolitan-apps-tools/ui'
 require 'digipolitan-apps-tools/file_utils'
 require 'digipolitan-apps-tools/xcodeproj'
 require 'digipolitan-apps-tools/fastlane'
